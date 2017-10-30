63a3e8f32872b102f7128e9b48da0595ae2c1c07
doylecm <32971939+doylecm@users.noreply.github.com>
2017-10-29 20:49:28 -0500

    Changes so far

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,27 +1,26 @@
-import java.math.BigInteger; // I know this is needed, but can't figure out how to make it work
-// I keep getting errors and have restarted this assignment numerous times.
+import java.math.BigInteger;
 
 public class LotteryOdds {
 
 	public static void main(String[] args) {
 
-		int r;
-		int w = 69; // 69 
-		int s = 5; // 5
-		int pb = 26;
-	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
+		BigInteger oddsofwinning = new BigInteger("292201338");
+		BigInteger whiteballs = new BigInteger("69");
+		BigInteger whiteballschosen = new BigInteger("5");
+		BigInteger redballs = new BigInteger("26");
+	    oddsofwinning = factorial(whiteballs).divide (factorial(whiteballschosen).multiply (factorial(whiteballs-whiteballschosen)));  // Superduper magic equation!!
+	    oddsofwinning = oddsofwinning.multiply (redballs);
 	    
-	    System.out.println("The odds of winning the PowerBall are 1:" + r);
+	    System.out.println("The odds of winning the PowerBall are 1:" + oddsofwinning);
 	    System.out.println("The PowerBall website says that it's 1:292201338");
 	    
     }
 
-	public static Integer factorial(int n) {
+	private static BigInteger factorial(BigInteger whiteballs) {
 	    int i = 1;
 	    int f = 1;
 	 
-	    for (int c = 1; c <= n; c++) {
+	    for (int c = 1; c <= whiteballs; c++) {
 	      f = f * i++;
 	    }
 

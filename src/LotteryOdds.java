8ab4ebd12f89a343edad3d4d110ddbcb3b115c92
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-04 22:16:48 -0500

    Revised
    I'm still not understanding how to make this work. Every time I try changing everything to BigInteger, it tells me that it can't convert from int BigInteger. So I change it back, but that is wrong too.

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,27 +1,27 @@
+
 import java.math.BigInteger;
 
 public class LotteryOdds {
 
 	public static void main(String[] args) {
+		
+		BigInteger R;
+		BigInteger W = BigInteger.valueOf (69);
+		BigInteger C = BigInteger.valueOf (5);
+		BigInteger PB = BigInteger.valueOf (26);
+		R = factorial(W.divide (factorial(C.multiply (factorial(W.min (C))))));
+		R = R.multiply (PB);
+		
+		System.out.println("The odds of winning the Powerball are 1:" + R);
+		System.out.println("The Powerball website says that it's 1:292,201,338");
+}
 
-		BigInteger oddsofwinning = new BigInteger("292201338");
-		BigInteger whiteballs = new BigInteger("69");
-		BigInteger whiteballschosen = new BigInteger("5");
-		BigInteger redballs = new BigInteger("26");
-	    oddsofwinning = factorial(whiteballs) / (factorial(whiteballschosen) * (factorial(whiteballs.min (whiteballschosen))));  // Superduper magic equation!!
-	    oddsofwinning = oddsofwinning.multiply (redballs);
-	    
-	    System.out.println("The odds of winning the PowerBall are 1:" + oddsofwinning);
-	    System.out.println("The PowerBall website says that it's 1:292201338");
-	    
-    }
-
-	private static int factorial(BigInteger whiteballs) {
-	   int i = 1;
-	   int f = 1;
-	   int c = 1;
-	    for (c = 1; c <= 69; c++) {
-	      f = f / (i++);
+	private static BigInteger factorial(BigInteger n) {
+	   BigInteger I = BigInteger.valueOf (1);
+	   BigInteger f = BigInteger.valueOf (1);
+	   BigInteger C = BigInteger.valueOf (1);
+	    for (C = 1; C <= (n); C++) {
+	      f = f / (BigInteger (I++);
 	    }
 
 		return f;
